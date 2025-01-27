523a22e1b865692c7bcc3ebf4a3e54eb981794e7
Draggor <draggor@gmail.com>
2025-01-26 23:02:29 -0600

    The beginnings of f2 2025 event listing.  Holy crap this thing needs to get rewritten, but hey, it mostly works for now.  Still need to clean up some dangling categories, need to clean up time display, need to get new header background image, titles, and colors.  NEED TO MAKE SAVE AND LOCAL STORAGE WORK!  We'll get there.

--- scripts/make-data.js
+++ scripts/make-data.js
@@ -1,25 +1,36 @@
 const fs = require('fs');
 const path = require('path');
 
+const { DateTime } = require('luxon');
 const parse = require('csv-parse/lib/sync');
 const PublicGoogleSheetsParser = require('public-google-sheets-parser');
 
 const basePath = path.resolve(__dirname, '..');
-const csvPath = path.resolve(basePath, 'data', 'dealers.csv');
+//const csvPath = path.resolve(basePath, 'data', 'dealers.csv');
+const csvPath = path.resolve(basePath, 'data', 'build.tab');
 const dataJsPath = path.resolve(basePath, 'src', 'data.js');
 
 
+const dayToDateOffset = {
+    'wednesday': 0,
+    'thursday': 1,
+    'friday': 2,
+    'saturday': 3,
+    'sunday': 4,
+    'monday': 5,
+};
+
 function writeData(dealers) {
-  const dataStr = `export default ${JSON.stringify(dealers)};`;
+    const dataStr = `export default ${JSON.stringify(dealers)};`;
 
-  fs.writeFileSync(dataJsPath, dataStr);
+    fs.writeFileSync(dataJsPath, dataStr);
 
-  console.log(`Writing ${dataJsPath} complete`);
+    console.log(`Writing ${dataJsPath} complete`);
 }
 
 function tagsStrToArray(tagsStr) {
  if (tagsStr) {
-   return tagsStr.split('||').map(sp => sp.trim()).filter(sp => sp.length > 0);
+     return tagsStr.split('||').map(sp => sp.trim()).filter(sp => sp.length > 0);
  }
 
  return [];
@@ -28,75 +39,86 @@ function tagsStrToArray(tagsStr) {
 
 // Reminder: 2nd arg of new Date() is month INDEX
 function getDateArgs(event) {
-  const day = parseInt(event.Day);
+    const day = dayToDateOffset[event.day];
 
-  if (!event.Start) return [2024, 1, 20+day];
+    //if (!event.Start) return [2025, 1, 20+day];
+    if (event.time.length == 0) return [2025, 1, 19];
 
-  const [hstr, mstr] = event.Start.split(':');
-  const hours = parseInt(hstr);
-  const minutes = parseInt(mstr);
+    const [hstr, mstr] = event.time.split(':');
+    const addTwelve = (event.time.indexOf('PM') > 0) ? 12 : 0;
+    const hours = parseInt(hstr) + addTwelve;
+    const minutes = parseInt(mstr.split(' ')[0]);
 
-  return [2024, 1, 20 + day, hours, minutes];
+    return [2025, 1, 19 + day, hours, minutes];
 }
 
 
-function formatSpeakers(speakers) {
-  if (!speakers) return [];
+function formatSpeakers(hosts, guests) {
+    /*
+    if (!speakers) return [];
 
-  return speakers.split('|').map(s => {
-    const i = s.indexOf(':');
+    return speakers.split('|').map(s => {
+        const i = s.indexOf(':');
 
-    return s.substr(i+1);
-  });
+        return s.substr(i+1);
+    });
+    */
+    if (guests) {
+        return hosts.split(', ').concat(guests.split(', '));
+    } else {
+        return hosts.split(', ');
+    }
 }
 
 
-function formatTime(event) {
-  let duration = '';
-
-  if (event.Duration) {
-    duration = `, Length ${event.Duration}`;
-  }
-
-  return `${event.Date} @ ${event.Start}${duration}`;
+function formatTime(dateObj, day, length) {
+    const time = dateObj.toLocaleTimeString('en-US', { hour: "2-digit", minute: "2-digit" });
+    return `${day} @ ${time}, ${length} minutes`;
 }
 
 
 function handleDealers(dealers) {
-  console.log(dealers[0]);
-  dealers.forEach((item, index) => {
-    item.tags = tagsStrToArray(item.Track);
-    item.Speakers = formatSpeakers(item.Speakers);
-    item.dateArgs = getDateArgs(item);
-    const dateObj = new Date(...item.dateArgs);
-    item.Date = dateObj.toLocaleString('en-us', { weekday:"long" });
-    item.Time = formatTime(item);
-    item.key = `_event_${index}_`;
-  });
+    console.log(dealers[4]);
+    transposedItems = dealers.map((item, index) => {
+        newItem = {};
+        //item.tags = tagsStrToArray(item.Track);
+        newItem.tags = [item.category];
+        newItem.Speakers = formatSpeakers(item.hosts, item.guests);
+        newItem.dateArgs = getDateArgs(item);
+        const dateObj = new Date(...newItem.dateArgs);
+        newItem.Date = dateObj.toLocaleString('en-us', { weekday:"long" });
+        newItem.Time = formatTime(dateObj, newItem.Date, item.length);
+        newItem.key = `_event_${index}_`;
+        newItem.Title = item.title;
+        newItem.Room = item.room;
+        newItem.Abstract = item.desc;
 
-  writeData(dealers);
+        return newItem;
+    });
+
+    writeData(transposedItems);
 }
 
 
 function processCsv() {
-  console.log(`Making src/data.js via csv file: ${csvPath}`);
-  const dealers_csv = fs.readFileSync(csvPath, {encoding: 'utf8'});
+    console.log(`Making src/data.js via csv file: ${csvPath}`);
+    const dealers_csv = fs.readFileSync(csvPath, {encoding: 'utf8'});
 
-  const dealers = parse(dealers_csv, {columns: true, skip_empty_lines: true});
+    const dealers = parse(dealers_csv, {delimiter: '\t', quote: false, relax_quotes: true, columns: true, skip_empty_lines: true});
 
-  handleDealers(dealers);
+    handleDealers(dealers);
 }
 
 function processGoogleSheet(spreadsheetId) {
-  console.log(`Making src/data.js via Google Sheet Id: ${spreadsheetId}`);
-  const parser = new PublicGoogleSheetsParser(spreadsheetId);
+    console.log(`Making src/data.js via Google Sheet Id: ${spreadsheetId}`);
+    const parser = new PublicGoogleSheetsParser(spreadsheetId);
 
-  parser.parse().then(handleDealers);
+    parser.parse().then(handleDealers);
 
 }
 
 if (process.env.SPREADSHEET_ID) {
-  processGoogleSheet(process.env.SPREADSHEET_ID);
+    processGoogleSheet(process.env.SPREADSHEET_ID);
 } else {
-  processCsv();
+    processCsv();
 }
